241b9709f5552c34c076b42db97ab4311b5c2ba2
jruttan1 <jruttan3@uwo.ca>
2026-03-29 19:21:35 -0400

    fixes and and add a task for the knn function

--- backend/embedding.py
+++ backend/embedding.py
@@ -3,6 +3,8 @@ client = OpenAI()
 from schemas import NoteCreate
 from sqlmodel import select
 from db import Note
+from typing import List
+from schemas import NoteResponse
 
 def create_embedding(note: NoteCreate):
     text = {'title': note.title,
@@ -14,12 +16,12 @@ def create_embedding(note: NoteCreate):
     embedding = response.data[0].embedding # unpack response object
     return embedding
 
-def get_k_nearest_neighbors(note: NoteCreate, session, k): # make sure to handle session when calling
+def get_k_nearest_neighbours(note: NoteCreate, session, k: int = 5) -> List[NoteResponse]: # make sure to handle session when calling
     search_embedding = create_embedding(note)
 
     knn = Note.embedding.cosine_distance(search_embedding)
-    statement = select(Note).order_by(knn).limit(k)
+    statement = select(Note, knn).order_by(knn).limit(k)
     notes = session.exec(statement)
 
-    return notes.all() 
+    return notes.all() # this will print as a list length k of tuples (Note, vector_distance)
 
--- backend/tasks.py
+++ backend/tasks.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from dotenv import load_dotenv
 import os
-from embedding import create_embedding
+from embedding import create_embedding, get_k_nearest_neighbours
 from schemas import NoteCreate, NoteResponse
 from sqlmodel import Session
 from db import engine
@@ -19,9 +19,20 @@ def embed_note(id: int) -> NoteResponse:
         note = get_note_from_id(id, session) # pull the note you wanna update
         embedding = create_embedding(note) # call create embedding, returns a list of vectors
         note.embedding = embedding # add it to the note object
-        
+
         session.add(note)
         session.commit()
         session.refresh(note)
     
         return note
+    
+@app.task
+def find_similar(id: int):
+    with Session(engine) as session:
+
+        note = get_note_from_id(id, session)
+        similar = get_k_nearest_neighbours(note, session) # pass k if you want more or less than 5 similar
+
+        # add logic for saving edges between nodes later
+
+        return similar
